0aa4ec840bfdb3bf86dd83bf240b042d0b6bdcd5
rafal <rafal@laptop.(none)>
2013-05-16 19:02:51 +0200

    widoki

---
+++ app/controllers/user/events_controller.rb
@@ -0,0 +1,40 @@
+class User::EventsController < User::UserController
+
+	def index
+		@events = Events.find(:all)
+	end
+
+	def show
+		@event = Event.find(params[:id])
+	end
+
+	def new
+		@event= Event.new
+	end
+
+	def create
+		@event = Event.new
+		@event.save!
+	end
+
+	def edit
+		@event = Event.find(params[:id])
+
+	end
+
+	def update
+		@event = Event.find(params[:id])
+		if @event.update_attributes(params[:event])
+        	redirect_to :action => 'show', :id => @event
+      	else         	
+         	render :action => 'edit'
+      	end
+
+	end
+
+	def delete 
+		Event.find(params[:id]).destroy
+     	redirect_to :action => 'index'
+	end
+
+end
